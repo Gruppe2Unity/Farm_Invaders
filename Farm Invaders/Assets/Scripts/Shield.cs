4746dffd7fe606603ecf0d20a4c8c80e38d0cce8
Gruppe2Unity <271914@student.usn.no>
2026-05-05 19:29:35 +0200

    Krav 8.3: Lagd sprites til skjold som dynamisk skal oppdateres dersom liv mistes, Endringer i Shield.cs for å rendere ny sprite dersom liv mistes. Bakgrunn endret for passe sammen med skjold.

--- Farm Invaders/Assets/Scripts/Shield.cs
+++ Farm Invaders/Assets/Scripts/Shield.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 
 /// <summary>
-/// Keeps track of shield health, and destruciton when health hits 0
-/// Shields protect the player from enemy bullets.
+/// Controls remaining shield health and destruction when shield is hit by bullets.
+/// Change sprites based on amount of health remaining
 /// </summary>
 public class Shield : MonoBehaviour
 {
-    [SerializeField] private int health = 3;
+    [SerializeField] private int health = 5;
+    [SerializeField] private Sprite[] damageSprites;
+
+    private SpriteRenderer spriteRenderer;
 
     /// <summary>
-    /// Detects collision with both Player bullets and Enemy bullets
+    /// Init of the sprite rendering as the game starts
+    /// </summary>
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// Detects collision with bullets and enemy bullets.
     /// </summary>
     /// <param name="collision">The collider that triggered the event.</param>
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,17 +32,29 @@ public class Shield : MonoBehaviour
     }
 
     /// <summary>
-    /// Makes shield take damage from bullets reducing health and destroying the shield when health hits 0
+    /// Reduces shield health, updates sprite and destroys it if health reaches zero.
     /// </summary>
     /// <param name="bullet">The bullet GameObject that hit the shield.</param>
     private void TakeDamage(GameObject bullet)
     {
         Destroy(bullet);
         health--;
+        UpdateSprite();
 
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Updates shield sprite based on remaining health.
+    /// </summary>
+    private void UpdateSprite()
+    {
+        if (health > 0 && health <= damageSprites.Length)
+        {
+            spriteRenderer.sprite = damageSprites[health - 1];
         }
     }
 }
